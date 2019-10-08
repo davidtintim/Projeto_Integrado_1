038772aa8bd39dceb148e6b6b5499136b01ecfcc
David Tintim <l40254@alunos.uevora.pt>
2019-10-08 14:36:08 +0100

    update

--- go/error-handling/eror_handling.go
+++ go/error-handling/eror_handling.go
@@ -8,3 +8,15 @@ func Use(o ResourceOpener, input string) (e error) {
 			return err
 		}
 	}
+defer func() {
+		if r := recover(); r != nil {
+			switch r.(type) {
+			case FrobError:
+				e = r.(FrobError).inner
+				result.Defrob(r.(FrobError).defrobTag)
+			case error:
+				e = r.(error)
+			default:
+				panic(r)
+			}
+		}
