9e684bf504cf5ccb368ffa51728d44bebc50a26b
David Tintim <l40254@alunos.uevora.pt>
2019-10-16 00:36:41 +0100

    update

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -30,3 +30,28 @@ func Verses(start, stop int) string {
 func Song() string {
 	return Verses(1, 8)
 }
+
+// first is the first line for a verse of "I know an old lady who swallowed a fly".
+var first = []string{
+	"",
+	"a fly.\n",
+	"a spider.\nIt wriggled and jiggled and tickled inside her.\n",
+	"a bird.\nHow absurd to swallow a bird!\n",
+	"a cat.\nImagine that, to swallow a cat!\n",
+	"a dog.\nWhat a hog, to swallow a dog!\n",
+	"a goat.\nJust opened her throat and swallowed a goat!\n",
+	"a cow.\nI don't know how she swallowed a cow!\n",
+	"a horse.\nShe's dead, of course!",
+}
+
+// refrain in the repeating lines in "I know an old lady who swallowed a fly"
+var refrain = []string{
+	"",
+	"I don't know why she swallowed the fly. Perhaps she'll die.",
+	"She swallowed the spider to catch the fly.\n",
+	"She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n",
+	"She swallowed the cat to catch the bird.\n",
+	"She swallowed the dog to catch the cat.\n",
+	"She swallowed the goat to catch the dog.\n",
+	"She swallowed the cow to catch the goat.\n",
+}
