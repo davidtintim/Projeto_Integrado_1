451719b4935fe5a4f396ce00ea0fb98b414c59c6
MIguel <carspower8@gmail.com>
2019-10-16 11:53:44 +0100

    Pi

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -1,1 +1,13 @@
 package summultiples
+
+func main(lim int,divisores ...int)(sum int){
+	for i :=1;i < lim;i++  {
+		for _, d := range divisores {
+			if d != 0 && i%d ==0{
+				sum +=1
+				break
+			}
+		}
+	}
+	return
+}
